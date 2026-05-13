f07ea0069d0cdee036cdaffc977f44318e4f4724
srivijayadharshini <srivijayadharshini.2024aids@sece.ac.in>
2026-05-13 11:56:37 +0530

    Add basic HTTP server to backend app

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Backend API Running" }));
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
